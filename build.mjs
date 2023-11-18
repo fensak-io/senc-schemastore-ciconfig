718461f693199c60f10c0c6faafb5fdcb35ccce6
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-11-17 20:01:20 -0600

    Use json-schema-to-typescript
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

---
+++ build.mjs
@@ -0,0 +1,23 @@
+// Copyright (c) Fensak, LLC.
+// SPDX-License-Identifier: MPL-2.0
+//
+// This contains a build script for generating a validation function based on a given jsonschema, using
+// json-schema-to-typescript.
+
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { compile } from "json-schema-to-typescript";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const resp = await fetch("https://json.schemastore.org/github-workflow.json");
+const schema = await resp.json();
+
+// Update the id, since it is used to generate the base type.
+schema["$id"] = "GitHubActionsWorkflowConfig";
+
+const types = await compile(schema, "GitHubActionsWorkflowConfig");
+fs.writeFileSync(path.join(__dirname, "index.d.ts"), types);
